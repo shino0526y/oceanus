8b4bf26a14c7e4523d7588c04a23bf439c40bd0a
Yamato SHINOZUKA <shino0526y@gmail.com>
2025-11-24 23:02:43 +0900

    chore: SCUが送信したデータを格納するためのDBのテーブル定義を作成

--- db/init/030-tables.sql
+++ db/init/030-tables.sql
@@ -8,3 +8,48 @@ CREATE TABLE application_entities(
     deleted_at timestamptz DEFAULT NULL
 );
 
+CREATE TABLE patients(
+    id varchar(16) PRIMARY KEY,
+    name_alphabet varchar(64) NOT NULL DEFAULT '',
+    name_kanji varchar(64) NOT NULL DEFAULT '',
+    name_hiragana varchar(64) NOT NULL DEFAULT '',
+    birth_date date,
+    sex smallint CHECK (sex >= 0 AND sex <= 2),
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    deleted_at timestamptz DEFAULT NULL
+);
+
+CREATE TABLE studies(
+    patient_id varchar(16) REFERENCES patients(id),
+    instance_uid varchar(64) PRIMARY KEY,
+    id varchar(16) NOT NULL DEFAULT '',
+    study_date date,
+    study_time time,
+    accession_number varchar(16) NOT NULL DEFAULT '',
+    ae_title varchar(16) NOT NULL REFERENCES application_entities(title),
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    deleted_at timestamptz DEFAULT NULL
+);
+
+CREATE TABLE series(
+    study_instance_uid varchar(64) NOT NULL REFERENCES studies(instance_uid),
+    instance_uid varchar(64) PRIMARY KEY,
+    modality varchar(16) NOT NULL DEFAULT '',
+    series_number integer,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    deleted_at timestamptz DEFAULT NULL
+);
+
+CREATE TABLE sop_instances(
+    series_instance_uid varchar(64) NOT NULL REFERENCES series(instance_uid),
+    class_uid varchar(64) NOT NULL DEFAULT '',
+    instance_uid varchar(64) PRIMARY KEY,
+    path text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    deleted_at timestamptz DEFAULT NULL
+);
+
