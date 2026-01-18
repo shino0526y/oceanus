3d8a397a2bc41218bcfaa93eb03d764407d28b1b
Yamato SHINOZUKA <shino0526y@gmail.com>
2026-01-18 23:13:11 +0900

    chore: デバッグ用の管理者ユーザーを登録するSQLを追加

---
+++ data/db/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(id, "name", "role", password_hash, created_at, updated_at)
+    VALUES ('admin', '管理者 太郎', 0, '$argon2id$v=19$m=19456,t=2,p=1$Zf/xy2I09QAEAvKnXga60w$arwk9jM50i/6RAjgZ2+N6fiRq0WWJFX3GmngTw+n34Y', '2026-01-18T22:53:24.334+0900', '2026-01-18T22:53:24.334+0900');
+
