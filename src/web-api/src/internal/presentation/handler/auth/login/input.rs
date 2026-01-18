2c5397254724616dd21efa82628a221a7e04ecea
Yamato SHINOZUKA <shino0526y@gmail.com>
2026-01-19 00:33:49 +0900

    feat: ログインAPIの入出力オブジェクトがcamelCaseでJSONシリアライズされるようにした

--- src/web-api/src/internal/presentation/handler/auth/login/input.rs
+++ src/web-api/src/internal/presentation/handler/auth/login/input.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 use utoipa::ToSchema;
 
 #[derive(Debug, Deserialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
 pub struct LoginInput {
     pub user_id: String,
     pub password: String,
--- src/web-api/src/internal/presentation/handler/auth/login/output.rs
+++ src/web-api/src/internal/presentation/handler/auth/login/output.rs
@@ -2,6 +2,7 @@ use serde::Serialize;
 use utoipa::ToSchema;
 
 #[derive(Debug, Serialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
 pub struct LoginOutput {
     pub csrf_token: String,
 }
