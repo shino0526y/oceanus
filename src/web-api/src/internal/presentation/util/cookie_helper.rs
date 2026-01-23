4999f6f4a602a8da9a268e4ee967720876fa7544
Yamato SHINOZUKA <shino0526y@gmail.com>
2026-01-23 22:27:41 +0900

    perf(web-api): 関数の引数を`String`から`Into<String>`に変更

--- src/web-api/src/internal/presentation/util/cookie_helper.rs
+++ src/web-api/src/internal/presentation/util/cookie_helper.rs
@@ -6,8 +6,11 @@ pub struct CookieHelper;
 impl CookieHelper {
     pub const SESSION_COOKIE_NAME: &'static str = "session_id";
 
-    pub fn create_session_cookie(session_id: String, max_age_minutes: i64) -> Cookie<'static> {
-        let mut cookie = Cookie::new(Self::SESSION_COOKIE_NAME, session_id);
+    pub fn create_session_cookie(
+        session_id: impl Into<String>,
+        max_age_minutes: i64,
+    ) -> Cookie<'static> {
+        let mut cookie = Cookie::new(Self::SESSION_COOKIE_NAME, session_id.into());
         cookie.set_path("/");
         cookie.set_http_only(true);
         cookie.set_same_site(SameSite::Strict);
