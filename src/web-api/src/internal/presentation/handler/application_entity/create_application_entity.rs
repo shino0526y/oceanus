0c6f1c87574aa0859b02f3dc015ccdc1c0f33144
Yamato SHINOZUKA <shino0526y@gmail.com>
2026-02-02 00:44:13 +0900

    test(web-api): `POST /application-entities` APIのテストを追加

--- src/web-api/src/internal/presentation/handler/application_entity/create_application_entity.rs
+++ src/web-api/src/internal/presentation/handler/application_entity/create_application_entity.rs
@@ -63,3 +63,300 @@ pub async fn create_application_entity(
 
     Ok(Json(output))
 }
+
+#[allow(non_snake_case)]
+#[cfg(test)]
+mod tests {
+    use crate::{
+        internal::presentation::handler::application_entity::prepare_test_data,
+        internal::presentation::util::test_helpers,
+        utils::{self, make_router},
+    };
+    use axum::{
+        body::{self, Body},
+        http::{Request, StatusCode},
+    };
+    use chrono::{DateTime, Utc};
+    use dicom_lib::core::value::value_representations::ae::AeValue;
+    use futures::future::JoinAll;
+    use serde_json::{Value, json};
+    use std::str::FromStr;
+    use tower::ServiceExt;
+    use uuid::Uuid;
+
+    #[tokio::test]
+    async fn 管理者はアプリケーションエンティティを作成できる() {
+        // Arrange
+        let repos = prepare_test_data().await;
+        let app_state = utils::make_app_state(&repos);
+        let router = make_router(app_state, &repos);
+        let (session_id, csrf_token) = test_helpers::login(&router, "admin", "Password#1234").await;
+        let input = json!({
+            "title": "TEST",
+            "host": "192.0.2.0",
+            "port": 104,
+            "comment": "",
+        });
+        let request = Request::builder()
+            .method("POST")
+            .uri("/application-entities")
+            .header("content-type", "application/json")
+            .header("cookie", format!("session_id={session_id}"))
+            .header("x-csrf-token", &csrf_token)
+            .body(Body::from(serde_json::to_string(&input).unwrap()))
+            .unwrap();
+
+        // Act
+        let response = router.clone().oneshot(request).await.unwrap();
+
+        // Assert
+        // HTTPレスポンスの確認
+        assert_eq!(response.status(), StatusCode::OK);
+        let bytes = body::to_bytes(response.into_body(), usize::MAX)
+            .await
+            .unwrap();
+        let body: Value = serde_json::from_slice(&bytes).unwrap();
+        assert_eq!(body["title"], "TEST");
+        assert_eq!(body["host"], "192.0.2.0");
+        assert_eq!(body["port"], 104);
+        assert_eq!(body["comment"], "");
+        let created_at = DateTime::from_str(body["createdAt"].as_str().unwrap()).unwrap();
+        let updated_at = DateTime::<Utc>::from_str(body["updatedAt"].as_str().unwrap()).unwrap();
+        let now = Utc::now();
+        assert!((now - created_at).num_seconds().abs() < 10);
+        assert_eq!(created_at, updated_at);
+        // リポジトリ内に正しく保存されていることの確認
+        let stored = repos
+            .application_entity_repository
+            .find_by_title(&AeValue::from_string("TEST").unwrap())
+            .await
+            .unwrap()
+            .unwrap();
+        assert_eq!(stored.host(), "192.0.2.0");
+        assert_eq!(stored.port().value(), 104);
+        assert_eq!(stored.comment(), "");
+        assert_eq!(
+            stored.created_by(),
+            &Uuid::from_str("019bdbbe-0dcc-7474-8b43-95b89ca8b4fd").unwrap()
+        );
+        assert_eq!(*stored.created_at(), created_at);
+        assert_eq!(*stored.updated_at(), updated_at);
+        assert!((now - *stored.updated_at()).num_seconds().abs() < 10);
+    }
+
+    #[tokio::test]
+    async fn 情シスはアプリケーションエンティティを作成できる() {
+        // Arrange
+        let repos = prepare_test_data().await;
+        let app_state = utils::make_app_state(&repos);
+        let router = make_router(app_state, &repos);
+        let (session_id, csrf_token) = test_helpers::login(&router, "it", "Password#1234").await;
+        let input = json!({
+            "title": "TEST",
+            "host": "192.0.2.0",
+            "port": 104,
+            "comment": "",
+        });
+        let request = Request::builder()
+            .method("POST")
+            .uri("/application-entities")
+            .header("content-type", "application/json")
+            .header("cookie", format!("session_id={session_id}"))
+            .header("x-csrf-token", &csrf_token)
+            .body(Body::from(serde_json::to_string(&input).unwrap()))
+            .unwrap();
+
+        // Act
+        let response = router.clone().oneshot(request).await.unwrap();
+
+        // Assert
+        // HTTPレスポンスの確認
+        assert_eq!(response.status(), StatusCode::OK);
+        let bytes = body::to_bytes(response.into_body(), usize::MAX)
+            .await
+            .unwrap();
+        let body: Value = serde_json::from_slice(&bytes).unwrap();
+        assert_eq!(body["title"], "TEST");
+        assert_eq!(body["host"], "192.0.2.0");
+        assert_eq!(body["port"], 104);
+        assert_eq!(body["comment"], "");
+        let created_at = DateTime::from_str(body["createdAt"].as_str().unwrap()).unwrap();
+        let updated_at = DateTime::<Utc>::from_str(body["updatedAt"].as_str().unwrap()).unwrap();
+        let now = Utc::now();
+        assert!((now - created_at).num_seconds().abs() < 10);
+        assert_eq!(created_at, updated_at);
+        // リポジトリ内に正しく保存されていることの確認
+        let stored = repos
+            .application_entity_repository
+            .find_by_title(&AeValue::from_string("TEST").unwrap())
+            .await
+            .unwrap()
+            .unwrap();
+        assert_eq!(stored.host(), "192.0.2.0");
+        assert_eq!(stored.port().value(), 104);
+        assert_eq!(stored.comment(), "");
+        assert_eq!(
+            stored.created_by(),
+            &Uuid::from_str("4922356e-d6a0-7083-8e18-93b7a023c328").unwrap()
+        );
+        assert_eq!(*stored.created_at(), created_at);
+        assert_eq!(*stored.updated_at(), updated_at);
+        assert!((now - *stored.updated_at()).num_seconds().abs() < 10);
+    }
+
+    #[tokio::test]
+    async fn 管理者でも情シスでもないユーザーがAEを作成しようとすると403エラーになる() {
+        // Arrange
+        let repos = prepare_test_data().await;
+        let app_state = utils::make_app_state(&repos);
+        let router = make_router(app_state, &repos);
+        let (session_id, csrf_token) =
+            test_helpers::login(&router, "technician", "Password#1234").await;
+        let input = json!({
+            "title": "TEST",
+            "host": "192.0.2.0",
+            "port": 104,
+            "comment": "",
+        });
+        let request = Request::builder()
+            .method("POST")
+            .uri("/application-entities")
+            .header("content-type", "application/json")
+            .header("cookie", format!("session_id={session_id}"))
+            .header("x-csrf-token", &csrf_token)
+            .body(Body::from(serde_json::to_string(&input).unwrap()))
+            .unwrap();
+
+        // Act
+        let response = router.clone().oneshot(request).await.unwrap();
+
+        // Assert
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+    }
+
+    #[tokio::test]
+    async fn すでに存在するAEと競合するAEを作成しようとすると409エラーになる() {
+        // Arrange
+        let repos = prepare_test_data().await;
+        let app_state = utils::make_app_state(&repos);
+        let router = make_router(app_state, &repos);
+        let (session_id, csrf_token) = test_helpers::login(&router, "admin", "Password#1234").await;
+        let inputs = [
+            json!({
+                "title": "DCMTK", // タイトルが既存と競合
+                "host": "192.0.2.0",
+                "port": 104,
+                "comment": "開発＆デバッグ用",
+            }),
+            json!({
+                "title": "TEST",
+                "host": "localhost", // ホスト名とポート番号の組が既存と競合
+                "port": 11112,
+                "comment": "開発＆デバッグ用",
+            }),
+        ];
+        let requests = inputs.iter().map(|input| {
+            Request::builder()
+                .method("POST")
+                .uri("/application-entities")
+                .header("content-type", "application/json")
+                .header("cookie", format!("session_id={session_id}"))
+                .header("x-csrf-token", &csrf_token)
+                .body(Body::from(serde_json::to_string(&input).unwrap()))
+                .unwrap()
+        });
+
+        // Act
+        let responses = requests
+            .map(async |request| router.clone().oneshot(request).await.unwrap())
+            .collect::<JoinAll<_>>()
+            .await;
+
+        // Assert
+        responses.into_iter().for_each(|response| {
+            assert_eq!(response.status(), StatusCode::CONFLICT);
+        });
+    }
+
+    #[tokio::test]
+    async fn リクエストボディのバリデーション違反の場合に422エラーになる() {
+        // Arrange
+        let repos = prepare_test_data().await;
+        let app_state = utils::make_app_state(&repos);
+        let router = make_router(app_state, &repos);
+        let (session_id, csrf_token) = test_helpers::login(&router, "admin", "Password#1234").await;
+        let inputs = [
+            json!({ // タイトルのフィールドがない
+                "host": "192.0.2.0",
+                "port": 104,
+                "comment": "",
+            }),
+            json!({ // タイトルが空文字
+                "title": "",
+                "host": "192.0.2.0",
+                "port": 104,
+                "comment": "",
+            }),
+            json!({ // タイトルが長すぎる(17文字)
+                "title": "12345678901234567",
+                "host": "192.0.2.0",
+                "port": 104,
+                "comment": "",
+            }),
+            json!({ // ホスト名のフィールドがない
+                "title": "TEST",
+                "port": 104,
+                "comment": "",
+            }),
+            json!({ // ホスト名が空文字
+                "title": "TEST",
+                "host": "",
+                "port": 104,
+                "comment": "",
+            }),
+            json!({ // ポート番号のフィールドがない
+                "title": "TEST",
+                "host": "192.0.2.0",
+                "comment": "",
+            }),
+            json!({ // ポート番号が不正(0)
+                "title": "TEST",
+                "host": "192.0.2.0",
+                "port": 0,
+                "comment": "",
+            }),
+            json!({ // ポート番号が不正(65536)
+                "title": "TEST",
+                "host": "192.0.2.0",
+                "port": 65536,
+                "comment": "",
+            }),
+            json!({ // コメントのフィールドがない
+                "title": "TEST",
+                "host": "192.0.2.0",
+                "port": 104,
+            }),
+        ];
+        let requests = inputs.iter().map(|input| {
+            Request::builder()
+                .method("POST")
+                .uri("/application-entities")
+                .header("content-type", "application/json")
+                .header("cookie", format!("session_id={session_id}"))
+                .header("x-csrf-token", &csrf_token)
+                .body(Body::from(serde_json::to_string(&input).unwrap()))
+                .unwrap()
+        });
+
+        // Act
+        let responses = requests
+            .map(async |request| router.clone().oneshot(request).await.unwrap())
+            .collect::<JoinAll<_>>()
+            .await;
+
+        // Assert
+        responses.into_iter().for_each(|response| {
+            assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+        });
+    }
+}
