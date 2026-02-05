03a7d0be242fac8e64136576586f1c7ace8355e7
Yamato SHINOZUKA <shino0526y@gmail.com>
2026-02-05 23:55:27 +0900

    test(web-api): `POST /logout` APIのテストを追加

--- src/web-api/src/internal/presentation/handler/auth/logout.rs
+++ src/web-api/src/internal/presentation/handler/auth/logout.rs
@@ -64,3 +64,51 @@ impl IntoResponse for LogoutError {
         (status, Json(error_response)).into_response()
     }
 }
+
+#[allow(non_snake_case)]
+#[cfg(test)]
+mod tests {
+    use super::super::prepare_test_data;
+    use crate::{internal::domain::entity::Session, startup};
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use tower::ServiceExt;
+    use uuid::Uuid;
+
+    #[tokio::test]
+    async fn ログインしている場合はログアウトできる() {
+        // Arrange
+        // 事前にセッションを作成しておく
+        let user_uuid = Uuid::parse_str("492236d4-2f18-76ab-a82f-84e29fcf92f8").unwrap();
+        let session = Session::create(user_uuid);
+        let session_id = session.session_id().to_string();
+        let csrf_token = session.csrf_token().to_string();
+        // リクエストの準備
+        let repos = prepare_test_data().await;
+        repos.session_repository.save(session).await;
+        let state = startup::make_state(&repos);
+        let router = startup::make_router(state, &repos);
+        let request = Request::builder()
+            .method("POST")
+            .uri("/logout")
+            .header("cookie", format!("session_id={session_id}"))
+            .header("x-csrf-token", csrf_token)
+            .body(Body::empty())
+            .unwrap();
+
+        // Act
+        let response = router.clone().oneshot(request).await.unwrap();
+
+        // Assert
+        // レスポンスの確認
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+        // リポジトリからセッションが削除されていることを確認
+        let session = repos
+            .session_repository
+            .find_by_session_id(&session_id)
+            .await;
+        assert!(session.is_none());
+    }
+}
